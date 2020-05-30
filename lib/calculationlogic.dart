d289db5a7cd6c13b1a4e34e52757927dfbda3409
Emmanuel Joshua <40811019+EmmanuelJoshua@users.noreply.github.com>
2020-05-30 02:05:18 +0100

    Final Commit.

--- lib/calculationlogic.dart
+++ lib/calculationlogic.dart
@@ -1,4 +1,4 @@
-// import 'package:function_tree/function_tree.dart';
+import 'package:function_tree/function_tree.dart';
 
 class Calculations {
   static const PERIOD = '.';
@@ -18,51 +18,27 @@ class Calculations {
     Calculations.PERCENT
   ];
 
-  static double add(double a, double b) => a + b;
-  static double subtract(double a, double b) => a - b;
-  static double divide(double a, double b) => a / b;
-  static double multiply(double a, double b) => a * b;
   static double percent(double a) => a / 100;
 }
 
 class CalculationLogic {
   static String parseString(String text) {
     List<String> numbersToAdd;
-    List<int> numbers = [];
-    double a, b, result;
-    if (text.contains(Calculations.ADD)) {
-      // final expressions = [text];
-      // for (final expression in expressions) {
-      //   // print(" '$expression' = ${expression.interpret()} ");
-      // }
-       numbersToAdd = text.split(Calculations.ADD);
-      a = double.parse(numbersToAdd[0]);
-      b = double.parse(numbersToAdd[1]);
-
-      result = Calculations.add(a, b);
-      // print(numbers);
-    } else if (text.contains(Calculations.MULTIPLY)) {
-      numbersToAdd = text.split(Calculations.MULTIPLY);
-      a = double.parse(numbersToAdd[0]);
-      b = double.parse(numbersToAdd[1]);
-
-      result = Calculations.multiply(a, b);
-    } else if (text.contains(Calculations.DIVIDE)) {
-      numbersToAdd = text.split(Calculations.DIVIDE);
-      a = double.parse(numbersToAdd[0]);
-      b = double.parse(numbersToAdd[1]);
-
-      result = Calculations.divide(a, b);
-    } else if (text.contains(Calculations.SUBTRACT)) {
-      numbersToAdd = text.split(Calculations.SUBTRACT);
-      a = double.parse(numbersToAdd[0]);
-      b = double.parse(numbersToAdd[1]);
-
-      result = Calculations.subtract(a, b);
+    double a, result;
+    double res;
+    if (text.contains(Calculations.ADD) ||
+        text.contains(Calculations.MULTIPLY) ||
+        text.contains(Calculations.DIVIDE) ||
+        text.contains(Calculations.SUBTRACT)) {
+      final expressions = [text];
+      for (final expression in expressions) {
+        res = double.parse(expression.interpret().toString());
+        print(" '$expression' = ${expression.interpret()} ");
+      }
+      result = res;
     } else if (text.contains(Calculations.PERCENT)) {
       numbersToAdd = text.split(Calculations.PERCENT);
-      a = double.parse(
-          numbersToAdd[0].substring(0, numbersToAdd[0].length - 1));
+      a = double.parse(numbersToAdd[0]);
 
       result = Calculations.percent(a);
       print(result);
@@ -74,24 +50,9 @@ class CalculationLogic {
       double parsedNumber = double.parse(result.toString());
 
       if ((parsedNumber != double.infinity) &&
-          text.contains(Calculations.MULTIPLY)) {
-        // parsedNumber.;
-        return parsedNumber.truncate().toString();
-      } else if ((parsedNumber != double.infinity) &&
-          text.contains(Calculations.DIVIDE)) {
-        if (parsedNumber == parsedNumber.floor()) {
-          return parsedNumber.truncate().toString();
-        } else {
-          return parsedNumber.toString();
-        }
-      } else if ((parsedNumber != double.infinity) &&
-          text.contains(Calculations.ADD)) {
-        if (parsedNumber == parsedNumber.floor()) {
-          return parsedNumber.truncate().toString();
-        } else {
-          return parsedNumber.toString();
-        }
-      } else if ((parsedNumber != double.infinity) &&
+              text.contains(Calculations.ADD) ||
+          text.contains(Calculations.MULTIPLY) ||
+          text.contains(Calculations.DIVIDE) ||
           text.contains(Calculations.SUBTRACT)) {
         if (parsedNumber == parsedNumber.floor()) {
           return parsedNumber.truncate().toString();
